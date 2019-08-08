b626f86c4ae7af6b966fc30bc0ffa0505441e389
mubashirm1 <--replace-all>
2019-08-08 22:00:40 +0530

    done

---
+++ q1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+echo"Hello What is your name??"
+read name 
+mkdir $simran
+touch $name/$simran.cpp
+echo"Your folder has been created"
+
---
+++ q2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Hi.....What is your name??"
+read name 
+mkdir $simran
+touch $name/$simran.cpp
+
+echo"Hello....Enter your name again....."
+read name2
+mkdir $simran
+cd $simran
+touch $simran1.cpp
+touch $simran2.cpp
+touch $simran3.cpp
+touch $simran4.cpp
+touch $simran5.cpp
+
